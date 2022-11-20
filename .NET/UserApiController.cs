9e0733f1e88196c47c57cd64d8e70b0f850eba4c
Miranda Merritt <merritt_miranda@rocketmail.com>
2022-11-19 19:22:53 -0700

    Add files via upload

---
+++ .NET/UserApiController.cs
@@ -0,0 +1,271 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Sabio.Models;
+using Sabio.Models.Domain;
+using Sabio.Models.Domain.Users;
+using Sabio.Models.Enums;
+using Sabio.Models.Requests;
+using Sabio.Services;
+using Sabio.Services.Interfaces;
+using Sabio.Web.Controllers;
+using Sabio.Web.Models.Responses;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Sabio.Web.Api.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserApiController : BaseApiController
+    {
+        private IUserService _service = null;
+        private IAuthenticationService<int> _authService = null;
+        private IEmailsService _emailsService = null;
+        public UserApiController(IEmailsService emailsService, IUserService service, ILogger<UserApiController> logger, IAuthenticationService<int> authenticationService) : base(logger)
+        {
+            _service = service;
+            _emailsService = emailsService;
+            _authService = authenticationService;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult<ItemResponse<int>> Create(UserAddRequest model)
+        {
+            int iCode = 201;
+            BaseResponse response = null;
+
+            try
+            {
+                int statusTypeId = (int)StatusTypes.Active;
+                int id = _service.Create(model, statusTypeId);
+
+                if (id > 0)
+                {
+                    string email = model.Email;
+                    int tokenTypeId =(int)TokenType.NewUser;
+                    string token = Guid.NewGuid().ToString();
+                    _service.AddUserToken(token, id, tokenTypeId);
+                    _emailsService.SendConfirmEmail(token, email);
+                    int customerRoleId = (int)Roles.Customer;
+                    _service.AddUserRole(id, customerRoleId);
+                }
+                if (id == 0)
+                {
+                    iCode = 404;
+                    response = new ErrorResponse("Application resource not found");
+                }
+                else
+                {
+                    response = new ItemResponse<int> { Item = id };
+                }
+            }
+            catch (Exception ex)
+            {
+                iCode = 500;
+                base.Logger.LogError(ex.ToString());
+                response = new ErrorResponse($"Generic Error: {ex.Message}");
+            }
+            return StatusCode(iCode, response);
+        }
+
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public async Task<ActionResult<SuccessResponse>> Login(UserLoginRequest model)
+        {
+            int code = 200;
+            BaseResponse response = null;
+            bool isValid = false;
+
+            try
+            {
+                isValid = await _service.LogInAsync(model.Email, model.Password);
+
+                if (isValid)
+                {
+                    response = new SuccessResponse();
+                }
+                else
+                {
+                    code = 404;
+                    response = new ErrorResponse("Credentials do not match anything in database");
+                }
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                Logger.LogError(ex.ToString());
+                response = new ErrorResponse(ex.Message);
+            }
+
+            return StatusCode(code, response);
+        }
+
+        [HttpGet("current")]
+        [AllowAnonymous]
+        public ActionResult<ItemResponse<IUserAuthData>> GetCurrrent()
+        {
+            int code = 200;
+            BaseResponse response = null;
+
+            try
+            {
+                IUserAuthData user = _authService.GetCurrentUser();
+
+                if (user == null)
+                {
+                    code = 404;
+                    response = new ErrorResponse("No current user found");
+                }
+                else
+                {
+                    response = new ItemResponse<IUserAuthData> { Item = user };
+                }
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                Logger.LogError(ex.ToString());
+                response = new ErrorResponse(ex.Message);
+            }
+
+            return StatusCode(code, response);
+        }
+
+        [HttpGet("logout")]
+        public async Task<ActionResult<SuccessResponse>> LogoutAsync()
+        {
+            int code = 200;
+            BaseResponse response = null;
+
+            try
+            {
+                await _authService.LogOutAsync();
+                response = new SuccessResponse();
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                Logger.LogError(ex.ToString());
+                response = new ErrorResponse(ex.Message);
+            }
+
+            return StatusCode(code, response);
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<ItemResponse<BaseUser>> GetById(int id)
+        {
+            int code = 200;
+            BaseResponse response = null;
+
+            try
+            {
+                BaseUser user = _service.GetById(id);
+
+                if (user == null)
+                {
+                    code = 404;
+                    response = new ErrorResponse("Application Resource not found");
+                }
+                else
+                {
+                    response = new ItemResponse<BaseUser> { Item = user };
+                }
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                Logger.LogError(ex.ToString());
+                response = new ErrorResponse(ex.Message);
+            }
+
+            return StatusCode(code, response);
+        }
+
+        [HttpGet("email")]
+        public ActionResult<ItemResponse<BaseUser>> GetIdByEmail (string address)
+        {
+            int code = 200;
+            int id = 0;
+            BaseResponse response = null;
+
+            try
+            {
+                id = _service.GetIdByEmail(address);
+                BaseUser user = _service.GetById(id);
+
+                if (user == null)
+                {
+                    code = 404;
+                    response = new ErrorResponse("Application Resource not found");
+                }
+                else
+                {
+                    response = new ItemResponse<BaseUser> { Item = user };
+                }
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                Logger.LogError(ex.ToString());
+                response = new ErrorResponse(ex.Message);
+            }
+
+            return StatusCode(code, response);
+        }
+
+        [HttpPut("confirm")]
+        [AllowAnonymous]
+        public ActionResult<SuccessResponse> Confirm(string token, string email)
+        {
+            int code = 200;
+            BaseResponse response = null;
+            try
+            {
+                _service.ConfirmUser(token, email);
+                response = new SuccessResponse();
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                response = new ErrorResponse(ex.Message);
+            }
+            return StatusCode(code, response);
+        }
+
+        [HttpGet("userFromToken")]
+        [AllowAnonymous]
+        public ActionResult<ItemResponse<int>> GetUserFromToken(int tokenTypeId, string token)
+        {
+            int code = 200;
+            BaseResponse response = null;
+            
+            try
+            {
+                int id = _service.GetUserFromToken(tokenTypeId, token);
+
+                if (id == 0)
+                {
+                    code = 404;
+                    response = new ErrorResponse("UserId not found");
+                }
+                else
+                {
+                    response = new ItemResponse<int> { Item = id } ;
+                }
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                response = new ErrorResponse(ex.Message);
+            }
+            return StatusCode(code, response);
+        }
+    }
+}
---
+++ .NET/UserService.cs
@@ -0,0 +1,265 @@
+﻿using Sabio.Data;
+using Sabio.Data.Providers;
+using Sabio.Models;
+using Sabio.Models.Domain;
+using Sabio.Models.Domain.Users;
+using Sabio.Models.Requests;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Sabio.Services
+{
+    public class UserService : IUserService
+    {
+        private IAuthenticationService<int> _authenticationService;
+        private IDataProvider _dataProvider;
+
+        public UserService(IAuthenticationService<int> authSerice, IDataProvider dataProvider)
+        {
+            _authenticationService = authSerice;
+            _dataProvider = dataProvider;
+        }
+
+        public async Task<bool> LogInAsync(string email, string password)
+        {
+            bool isSuccessful = false;
+
+            IUserAuthData response = GetCurrent(email, password);
+
+            if (response != null)
+            {
+                await _authenticationService.LogInAsync(response);
+                isSuccessful = true;
+            }
+
+            return isSuccessful;
+        }
+
+        public async Task<bool> LogInTest(string email, string password, int id, string[] roles = null)
+        {
+            bool isSuccessful = false;
+            var testRoles = new[] { "User", "Super", "Content Manager" };
+
+            var allRoles = roles == null ? testRoles : testRoles.Concat(roles);
+
+            IUserAuthData response = new UserBase
+            {
+                Id = id
+                ,
+                Name = email
+                ,
+                Roles = allRoles
+                ,
+                TenantId = "Acme Corp UId"
+            };
+
+            Claim fullName = new Claim("CustomClaim", "Sabio Bootcamp");
+            await _authenticationService.LogInAsync(response, new Claim[] { fullName });
+
+            return isSuccessful;
+        }
+
+        public int Create(UserAddRequest model, int statusTypeId)
+        {
+            int userId = 0;
+            string password = model.Password;
+            string salt = BCrypt.BCryptHelper.GenerateSalt();
+            string hashedPassword = BCrypt.BCryptHelper.HashPassword(password, salt);
+            string procName = "[dbo].[Users_Insert]";
+
+            _dataProvider.ExecuteNonQuery(procName,
+                inputParamMapper: delegate (SqlParameterCollection col)
+                {
+                    AddCommonParams(model, col, hashedPassword);
+                    col.AddWithValue("@statusTypeId", statusTypeId);
+                    SqlParameter idOut = new SqlParameter("@Id", SqlDbType.Int);
+                    idOut.Direction = ParameterDirection.Output;
+                    col.Add(idOut);
+                },
+            returnParameters: delegate (SqlParameterCollection returnCollection)
+            {
+                object oId = returnCollection["@Id"].Value;
+                int.TryParse(oId.ToString(), out userId);
+            }
+            );
+
+            return userId;
+        }
+
+        public IUserAuthData GetCurrent(string email, string password)
+        {
+            string procName = "[dbo].[Users_Select_AuthData]";
+            UserBase user = null;
+            AuthUser authUser = null;
+
+            _dataProvider.ExecuteCmd(procName, inputParamMapper: delegate (SqlParameterCollection col)
+            {
+                col.AddWithValue("@Email", email);
+            }, singleRecordMapper: delegate (IDataReader reader, short set)
+            {
+
+                if (set == 0)
+                {
+                    int startingIndex = 0;
+
+                    authUser = new AuthUser();
+                    user = new UserBase();
+
+                    authUser.Id = reader.GetSafeInt32(startingIndex++);
+                    authUser.Email = reader.GetSafeString(startingIndex++);
+                    authUser.Password = reader.GetSafeString(startingIndex++);
+
+                    bool isValidCredentials = BCrypt.BCryptHelper.CheckPassword(password, authUser.Password);
+
+                    if (isValidCredentials)
+                    {
+                        user.Id = authUser.Id;
+                        user.Name = authUser.Email;
+                        user.TenantId = "Immersed";
+                    }
+                }
+                if (set == 1)
+                {
+                    if (authUser.Roles == null)
+                    {
+                        authUser.Roles = new List<string>();
+                    }
+                    var role = reader.GetSafeString(0);
+                    authUser.Roles.Add(role);
+                }
+                user.Roles = authUser.Roles;
+            }
+            );
+
+            return user;
+        }
+
+        public BaseUser GetById(int id)
+        {
+            string procName = "[dbo].[Users_Select_ById]";
+            BaseUser baseUser = null;
+
+            _dataProvider.ExecuteCmd(procName, delegate (SqlParameterCollection paramCollection)
+            {
+                paramCollection.AddWithValue("@Id", id);
+
+            },
+            delegate (IDataReader reader, short set)
+            {
+                baseUser = MapSingleUser(reader);
+            }
+            );
+
+            return baseUser;
+        }
+
+        public int GetIdByEmail(string email)
+        {
+            string procName = "[dbo].[Users_SelectId_ByEmail]";
+            int baseUserId = 0;
+
+            _dataProvider.ExecuteCmd(procName, delegate (SqlParameterCollection paramCollection)
+            {
+                paramCollection.AddWithValue("@Email", email);
+            },
+            delegate (IDataReader reader, short set)
+            {
+                int startingIndex = 0;
+                baseUserId = reader.GetSafeInt32(startingIndex++);
+            });
+
+            return baseUserId;
+        }
+
+        public void AddUserToken(string token, int userId, int tokenTypeId)
+        {
+            string procName = "[dbo].[UserTokens_Insert]";
+
+            _dataProvider.ExecuteNonQuery(procName
+                , inputParamMapper: delegate (SqlParameterCollection col)
+                {
+                    col.AddWithValue("@Token", token);
+                    col.AddWithValue("@UserId", userId);
+                    col.AddWithValue("@TokenTypeId", tokenTypeId);
+                }
+                );
+        }
+
+        public void AddUserRole(int userId, int roleId)
+        {
+            string procName = "[dbo].[UserRoles_Insert]";
+
+            _dataProvider.ExecuteNonQuery(procName
+                , inputParamMapper: delegate (SqlParameterCollection col)
+                {
+                    col.AddWithValue("@UserId", userId);
+                    col.AddWithValue("@RoleId", roleId);
+                }
+                );
+        }
+
+        public void ConfirmUser(string token, string email)
+        {
+            string procName = "[dbo].[Users_Confirm]";
+
+            _dataProvider.ExecuteNonQuery(procName
+                , inputParamMapper: delegate (SqlParameterCollection col)
+                {
+                    col.AddWithValue("@Email", email);
+                    col.AddWithValue("@Token", token);
+                }
+                );
+        }
+
+        public int GetUserFromToken(int tokenTypeId, string token)
+        {
+            int userId = 0;
+
+            string procName = "[dbo].[UserTokens_Select_ByTokenAndTokenTypeId]";
+
+            _dataProvider.ExecuteCmd(procName
+                , inputParamMapper: delegate (SqlParameterCollection col)
+                {
+                    col.AddWithValue("@TokenTypeId", tokenTypeId);
+                    col.AddWithValue("@Token", token);
+                },
+                delegate (IDataReader reader, short set)
+                {
+                    userId = reader.GetSafeInt32(0);
+                }
+                );
+                return userId;
+        }
+
+        private static BaseUser MapSingleUser(IDataReader reader)
+        {
+            BaseUser aUser = new BaseUser();
+
+            int startingIndex = 0;
+
+            aUser.Id = reader.GetSafeInt32(startingIndex++);
+            aUser.Email = reader.GetSafeString(startingIndex++);
+            aUser.FirstName = reader.GetSafeString(startingIndex++);
+            aUser.LastName = reader.GetSafeString(startingIndex++);
+            aUser.Mi = reader.GetSafeString(startingIndex++);
+            aUser.AvatarUrl = reader.GetSafeString(startingIndex++);
+            return aUser;
+        }
+
+        private static void AddCommonParams(UserAddRequest model, SqlParameterCollection col, string password)
+        {
+            col.AddWithValue("@Email", model.Email);
+            col.AddWithValue("@FirstName", model.FirstName == null ? DBNull.Value : model.FirstName);
+            col.AddWithValue("@LastName", model.LastName == null ? DBNull.Value : model.LastName);
+            col.AddWithValue("@Mi", model.Mi == null ? DBNull.Value : model.Mi);
+            col.AddWithValue("@AvatarUrl", model.AvatarUrl == null ? DBNull.Value : model.AvatarUrl);
+            col.AddWithValue("@Password", password);
+        }
+    }
+}
